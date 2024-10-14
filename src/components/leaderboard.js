54ebb75b07a8590f4502f697568f39f203cdc891
patrickwoodhead <patrick.woodhead@protocol.ai>
2024-10-13 20:24:15 -0400

    remove example files

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -2,7 +2,10 @@ import * as Plot from "npm:@observablehq/plot";
 
 export function Leaderboard(records) {
     const sortedAndPaginatedRecords = records.slice(0,20)
-    <div class="card" style="padding: 0;">
-    ${Inputs.table(baHourlyClean, {rows: 16})}
-  </div>
+    return Plot.plot({
+        y: {padding: 0.4},
+        marks: [
+            Plot.barX(sortedAndPaginatedRecords, {x: "success_rate", y: "miner_id"})
+        ]
+    })
 }
