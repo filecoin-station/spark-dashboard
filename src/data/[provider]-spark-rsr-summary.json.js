b0bf6a12edc243b53f71f340ba1f3b2349d5d197
Srdjan S <stankovic.srdjo@gmail.com>
2024-12-05 12:12:35 +0100

    Add retry to fetching provider rsr summary

--- src/data/[provider]-spark-rsr-summary.json.js
+++ src/data/[provider]-spark-rsr-summary.json.js
@@ -1,3 +1,4 @@
+import pRetry from 'p-retry';
 import { jsonFetcher } from "./json-fetcher.js";
 import { getDateXDaysAgo } from "../utils/date-utils.js";
 import { parseArgs } from "node:util";
@@ -11,9 +12,6 @@ const {
 const start = '2024-04-07';
 const end = getDateXDaysAgo(1);
 
-const summary = await jsonFetcher(
-  `https://stats.filspark.com/miner/${provider}/retrieval-success-rate/summary?from=${start}&to=${end}`
-);
-
+const summary = await pRetry(() => jsonFetcher(`https://stats.filspark.com/miner/${provider}/retrieval-success-rate/summary?from=${start}&to=${end}`), {retries: 3});
 
 process.stdout.write(JSON.stringify(summary));
