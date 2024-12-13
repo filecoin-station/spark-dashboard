f3a5fbad5f4c9cd1e54e35169198af8b397b4947
Srdjan S <stankovic.srdjo@gmail.com>
2024-12-13 12:55:17 +0100

    Add authorization token to request header

--- src/data/json-fetcher.js
+++ src/data/json-fetcher.js
@@ -1,7 +1,11 @@
 import { AbortError } from "p-retry";
 
 export async function jsonFetcher(url) {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${process.env.API_TOKEN}`,
+        }
+    });
     // Abort retrying if the resource doesn't exist
     if (response.status === 404) throw new AbortError(response.statusText);
     if (!response.ok) throw new Error(`fetch failed: ${response.status}`);
