d16bde96d0f260426b288308cdaffe5974f0c119
Nikolas Haimerl <nikolas.haimerl@dfinity.org>
2025-01-11 14:38:08 +0100

    add histogram

--- src/components/histogram.js
+++ src/components/histogram.js
@@ -1,16 +1,49 @@
-import * as Plot from "npm:@observablehq/plot";
+import * as Plot from 'npm:@observablehq/plot'
+import * as d3 from 'd3'
 
-export function Histogram(events, { width, title, thresholds }) {
-    return Plot.plot({
-        y: {grid: true },
-        x: { percent: true },
-        title,
-        width,
-        marks: [
-        Plot.rectY(events, Plot.binX({
-            y: "count", title: (elems) =>  `${elems.length} SPs`},
-            {x: {thresholds, value: "success_rate"}, fill: "#5A3DFF"})),
-        Plot.ruleY([0])
-        ],
-    })
+export function Histogram (events, { width, title, thresholds }) {
+  const data = events.flatMap(d => [
+    { type: 'Successful Http', value: d.success_rate * 100 },
+    { type: 'Successful', value: d.success_rate * 100 }
+  ])
+
+  const binnedData = Array.from(new Set(data.map(item => item.type))).flatMap(type => {
+    const groupData = data.filter(d => d.type === type)
+    const bins = d3
+      .bin()
+      .domain([0, 100])
+      .thresholds(thresholds)(groupData.map(d => d.value))
+
+    return bins.map(bin => ({
+      type,
+      threshold: `${bin.x0}–${bin.x1}`,
+      count: bin.length
+    }))
+  })
+
+  return Plot.plot({
+    marks: [
+      Plot.barY(
+        binnedData,
+        {
+          fx: 'threshold',
+          y: 'count',
+          x: 'type',
+          fill: 'type'
+        }
+      )
+    ],
+    y: { grid: true },
+    x: {
+      axis: null,
+      padding: 0.1
+    },
+    color: {
+      legend: true,
+      label: 'Type'
+    },
+    width,
+    title,
+    facet: { label: 'Rate Ranges' }
+  })
 }
--- src/components/line-graph.js
+++ src/components/line-graph.js
@@ -1,26 +1,45 @@
-import * as Plot from "npm:@observablehq/plot";
+import * as Plot from 'npm:@observablehq/plot'
 
-export function LineGraph(events, {width, height, title, start, end} = {}) {
-    const startDate = new Date(start);
-    const endDate = new Date(end);
-    const filteredEvents = events.filter((event) => {
-        const eventDate = new Date(event.day)
-        return eventDate >= startDate && eventDate < endDate
-    })
-    return Plot.plot({
-        title,
-        width,
-        height,
-        x: {type: "utc", ticks: "month", label: null},
-        y: {grid: true, inset: 10, label: "RSR (%)", percent: true},
-        marks: [
-            Plot.lineY(filteredEvents, {
-                x: "day",
-                y: "success_rate",
-                // z: null, // varying color, not series
-                stroke: "#FFBD3F",
-                // curve: ""
-            })
-        ]
-    })
+export function LineGraph (events, { width, height, title, start, end } = {}) {
+  const startDate = new Date(start)
+  const endDate = new Date(end)
+  const filteredEvents = events.filter((event) => {
+    const eventDate = new Date(event.day)
+    return eventDate >= startDate && eventDate < endDate
+  })
+  const combinedData = [
+    ...filteredEvents.map((event) => ({
+      day: event.day,
+      success_rate: event.success_rate,
+      type: 'Successful'
+    })),
+    ...filteredEvents.map((event) => ({
+      day: event.day,
+      success_rate_http: event.success_rate / 2,
+      type: 'Successful HTTP'
+    }))
+  ]
+
+  return Plot.plot({
+    title,
+    width,
+    height,
+    x: { type: 'utc', ticks: 'month', label: null },
+    y: { grid: true, inset: 10, label: 'RSR (%)', percent: true },
+    color: { legend: true },
+    marks: [
+      Plot.lineY(combinedData, {
+        x: 'day',
+        y: 'success_rate',
+        stroke: 'type',
+        curve: 'linear'
+      }),
+      Plot.lineY(combinedData, {
+        x: 'day',
+        y: 'success_rate_http',
+        stroke: 'type',
+        curve: 'linear'
+      })
+    ]
+  })
 }
