51d37e23d3170094c0b06239ad504e26340926bc
Srdjan <stankovic.srdjo@gmail.com>
2025-01-16 15:44:24 +0100

    Update src/data/spark-retrieval-timings.json.js
    Co-authored-by: Miroslav Bajtoš <oss@bajtos.net>

--- src/data/spark-retrieval-timings.json.js
+++ src/data/spark-retrieval-timings.json.js
@@ -2,7 +2,7 @@ import pRetry from 'p-retry';
 import { jsonFetcher } from "./json-fetcher.js";
 import { getDateXDaysAgo } from "../utils/date-utils.js";
 
-const start = '2024-04-07';
+const start = '2025-01-01';
 const end = getDateXDaysAgo(1);
 
 const summary = await pRetry(() => jsonFetcher(`https://stats.filspark.com/retrieval-timings/daily?from=${start}&to=${end}`), { retries: 3 });
