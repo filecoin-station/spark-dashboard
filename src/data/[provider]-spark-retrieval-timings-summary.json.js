3685ce338a8f2a8f4853e59571222c04e3b20cc7
Srdjan S <stankovic.srdjo@gmail.com>
2025-01-16 21:09:38 +0100

    Remove faked datapoints

--- src/data/[provider]-spark-retrieval-timings-summary.json.js
+++ src/data/[provider]-spark-retrieval-timings-summary.json.js
@@ -14,5 +14,4 @@ const end = getDateXDaysAgo(1);
 
 const summary = await pRetry(() => jsonFetcher(`https://stats.filspark.com/miner/${provider}/retrieval-timings/summary?from=${start}&to=${end}`), { retries: 3 });
 
-// fake datapoint on January 13th so chart starts from 0
-process.stdout.write(JSON.stringify([{ day: '2025-01-13', ttfb_ms: 0 }, ...summary]));
+process.stdout.write(JSON.stringify(summary));
--- src/data/spark-retrieval-timings.json.js
+++ src/data/spark-retrieval-timings.json.js
@@ -7,5 +7,4 @@ const end = getDateXDaysAgo(1);
 
 const summary = await pRetry(() => jsonFetcher(`https://stats.filspark.com/retrieval-timings/daily?from=${start}&to=${end}`), { retries: 3 });
 
-// fake datapoint on January 13th so chart starts from 0
-process.stdout.write(JSON.stringify([{ day: '2025-01-13', ttfb_ms: 0 }, ...summary]));
+process.stdout.write(JSON.stringify(summary));
